e87f1704f8cda2328cc53a62d6b8a371d3792c8e
Abhi3739 <abhibindass37@gmail.com>
2022-02-05 21:41:25 +0530

    bootstrap import

--- secdev-application/src/App.tsx
+++ secdev-application/src/App.tsx
@@ -20,7 +20,6 @@ const Groups = lazy(() => import('./components/Groups'));
 const Events = lazy(() => import('./components/Events'));
 const Navbar = lazy(() => import('./components/Navigation/Navbar'));
 const SideDrawer = lazy(() => import('./components/Navigation/SideDrawer'));
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const useStyles = makeStyles({
   root: {
