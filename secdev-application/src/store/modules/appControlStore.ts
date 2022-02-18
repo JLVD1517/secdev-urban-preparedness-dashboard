1b27d3aa6173909a5a685091659ad49e36103857
Jamie Harnum <j.harnum@secdev.com>
2022-02-17 20:29:10 -0500

    Default to light mode

--- secdev-application/src/store/modules/appControlStore.ts
+++ secdev-application/src/store/modules/appControlStore.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { InitialStateAppControl } from '../../types';
 
 const appControlInitialState: InitialStateAppControl = {
-  darkTheme: true,
+  darkTheme: false,
   sideNavOpen: false,
 };
 
